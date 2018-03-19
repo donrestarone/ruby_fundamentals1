f545d953b7bc309c7a2349147e3a383f541205b5
donrestarone <shashikejayatunge@gmail.com>
2018-03-19 14:29:03 -0400

    complete exercise 4.2

---
+++ exercise4-2.rb
@@ -0,0 +1,11 @@
+puts "please enter your age"
+	age = gets.chomp.to_i 
+		if age > 105 
+			puts "i dont think i believe you"
+		end
+	myage = 23
+	delta = age - myage
+
+puts "the age differendce is #{delta}"
+
+# delta.to_s
