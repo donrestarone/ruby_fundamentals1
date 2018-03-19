ae995e46bc6c5ececf9cfca48ba23d7f47eae8fa
donrestarone <shashikejayatunge@gmail.com>
2018-03-19 14:06:06 -0400

    start second exercise

---
+++ exercise2.rb
@@ -0,0 +1,16 @@
+#question 1 
+
+puts "enter meal price"
+	mealprice = gets.chomp.to_f
+	tip = mealprice * 0.13
+
+	puts "the appropriate tip is $#{tip}" 
+#question 2 
+	puts "price is $" + tip.to_s 
+
+	a = 45628
+	b = 7839
+	c = a + b 
+	#question 3 
+	 puts "the result of 45628 multiplied by 7839 is #{c}"
+
