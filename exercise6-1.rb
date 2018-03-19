22d057044ed5b9c4734926b3ac75f26dd00d55bf
donrestarone <shashikejayatunge@gmail.com>
2018-03-19 16:15:55 -0400

    complete exercise 6.1

---
+++ exercise6-1.rb
@@ -0,0 +1,24 @@
+distance =  0 
+input = ""
+	
+	while distance >= 0 && input != "go home"
+
+		p "would you like to walk or run"
+		input = gets.chomp 
+
+		if input == "walk"
+			distance += 1
+			# distance = distance + 1 
+		
+		elsif input == "run" 
+			distance += 5 
+				# distance = distance + 5
+		
+		else
+			p "i dont know"
+		
+		end 
+		
+		puts "#{distance.to_s}"
+	end 
+
