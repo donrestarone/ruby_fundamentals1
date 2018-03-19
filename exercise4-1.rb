89f2f57e93878a98c3458f4d50b809dbce46abd0
donrestarone <shashikejayatunge@gmail.com>
2018-03-19 14:24:12 -0400

    complete exercise 4.1

---
+++ exercise4-1.rb
@@ -0,0 +1,9 @@
+puts "please enter a number "
+	number = gets.chomp.to_i 
+	if number < 100 
+		puts "why not dream a little bigger?"
+	else number >= 100 
+			puts "whoa thats a big number!"
+
+	end 
+		
